9c9a3aee0af6f360673a2d9620833af149db4f46
HudsonWayne <waynewaynebenhura@gmail.com>
2025-04-29 12:07:44 +0200

    added html page

--- app.py
+++ app.py
@@ -10,5 +10,10 @@ def home():
 def about():
     return "This is about page"
 
+@app.route("/html")
+def html_page():
+    return "<h1>Welcome to Flask</h1><p>This is a paragraph.</p>"
+
+
 if __name__ == "__main__":
     app.run(debug=True)
